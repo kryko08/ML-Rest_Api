e61bf68b451d6741a24b256f269ac9b2dbcb1f18
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-20 12:35:58 +0200

    Add predictor to apps file

--- ModelApi/apps.py
+++ ModelApi/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from .predictor import NTSNetPredictor
 
 class ModelapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ModelApi'
+
+    # Load Model here
+    ML_MODEL = NTSNetPredictor
