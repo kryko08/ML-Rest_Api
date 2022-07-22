52ffbc24c2ae7958e213f9ec0ea6e7d9398f2d20
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-22 14:29:55 +0200

    request_model_view added

--- ModelApi/views.py
+++ ModelApi/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import get_object_or_404
 
 from rest_framework.generics import (CreateAPIView,
                                      ListAPIView, 
@@ -16,13 +17,48 @@ from .serializers import AlgorithmRequestSerializer
 from .serializers import UserDetailSerializer, UserSerializer
 from django.contrib.auth.models import User
 
+from rest_framework.decorators import api_view
+
+from .apps import ModelapiConfig
+
+from rest_framework.response import Response
+
+
 
 # Request to Algorithm view
-class RequestCreateView():
-    pass
+@api_view(["GET", "POST"])
+def request_model_view(request, pk=None, *args, **kwargs):
+    # If HTTP method is "POST", run model inference and show results
+    if request.method == "POST":
+        serializer = AlgorithmRequestSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            # Run inference
+            validated_data = serializer.validated_data
+            message = validated_data["request_message"]
+            model_output = ModelapiConfig.predictor.translate(message)
+            serializer.save(response=model_output)
+        else:
+            return Response()
 
-class RequestListView():
-    pass
+    # If HTTP method is "Get", view all previous algorithm requests
+    elif request.method == "GET":
+        # Detail view 
+        if pk is not None:
+            obj = get_object_or_404(AlgorithmRequest, pk=pk)
+            data = AlgorithmRequestSerializer(obj, many=False).data
+            return Response(data)
+        # List view
+        queryset = AlgorithmRequest.objects.all()
+        data = AlgorithmRequestSerializer(queryset, many=True).data
+        return Response(data)
+
+
+
+# View for listing all requests 
+class RequestListView(ListAPIView):
+    queryset = AlgorithmRequest.objects.all()
+    serializer_class = AlgorithmRequestSerializer
+    
 
 # User views 
 class ListUserView(ListAPIView):
