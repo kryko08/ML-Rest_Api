1f21717f2b9d3f0de21da19d5461c8aa782b0190
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-22 14:29:34 +0200

    imports fixed

--- ModelApi/urls.py
+++ ModelApi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (ListUserView, 
                     UserDetailView,
-                    RequestCreateView,
+                    request_model_view,
                     RequestListView,
                    )
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path("users/", ListUserView.as_view()),
     path("users/<int:pk>/", UserDetailView.as_view()),
     # Request machine learning model prediction 
-    path("predict/", RequestCreateView.as_view()),
+    path("predict/", request_model_view),
     # View all requests to predictor 
     path("predictions/", RequestListView.as_view())
 ]
