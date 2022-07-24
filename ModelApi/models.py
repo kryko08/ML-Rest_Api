a7338d3a07a9cbfddf9aa5614ea15c73bef45093
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-25 00:56:23 +0200

    __str__ dunder method added, charfield "max_lenght" attribute changed

--- ModelApi/models.py
+++ ModelApi/models.py
@@ -10,9 +10,11 @@ from datetime import datetime
 now = datetime.now()
 
 
-
 class AlgorithmRequest(models.Model):
-    requested_at = models.DateTimeField(auto_now_add=True)
+    requested = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    request_message = models.CharField(max_length=200)
-    response = models.CharField(max_length=150)
+    request_message = models.CharField(max_length=150)
+    response = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.request_message
