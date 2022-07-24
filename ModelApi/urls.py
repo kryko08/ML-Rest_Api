429787ddf1419bd2fabb7941b6c47ba99991ad4a
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-25 00:57:23 +0200

    url paths modified

--- ModelApi/urls.py
+++ ModelApi/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from .views import (ListUserView, 
                     UserDetailView,
-                    request_model_view,
-                    RequestListView,
+                    AlgoRequestMixinView,
+                    AlgoRequestDetailView,
+                    UserPredictionListView,
                    )
 
 urlpatterns = [
     # Users 
     path("users/", ListUserView.as_view()),
-    path("users/<int:pk>/", UserDetailView.as_view()),
+    path("users/<int:pk>/detail", UserDetailView.as_view(), name="user-detail"),
+    path("users/<int:pk>/predictions", UserPredictionListView.as_view(), name="user-prediction-list"),
     # Request machine learning model prediction 
-    path("predict/", request_model_view),
-    # View all requests to predictor 
-    path("predictions/", RequestListView.as_view())
+    path("predict/", AlgoRequestMixinView.as_view()),
+    path("predict/<int:pk>/detail/", AlgoRequestDetailView.as_view(), name="algorithmrequest-detail"),
+    # Predictions views 
 ]
